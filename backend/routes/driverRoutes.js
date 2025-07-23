edca18e39d57f7cd5bc0f5caf91b8acd60716ba5
shivikagarg999 <shivikagarg91@gmail.com>
2025-07-23 19:40:34 +0530

    Middleware added in route

--- backend/routes/driverRoutes.js
+++ backend/routes/driverRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { driverLogin, getDriverProfile } = require('../controllers/driver/auth');
+const verifyDriverToken = require('../middleware/driverAuth')
+
 
 router.post('/login', driverLogin);
-router.get('/profile',getDriverProfile )
+router.get('/profile',verifyDriverToken,getDriverProfile )
 
 module.exports = router;
